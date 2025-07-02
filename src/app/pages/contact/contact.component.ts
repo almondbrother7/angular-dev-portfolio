6bbb0cfad5cc1fe61c3a705c5769e445e9d2382a
Andrew Carey <almondbrother7@gmail.com>
2025-07-02 01:15:50 -0400

    Added Firebase Function verifyRecaptcha

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+declare const grecaptcha: any;
 
 @Component({
   selector: 'app-contact',
@@ -11,5 +12,29 @@ import { RouterModule } from '@angular/router';
   styleUrl: './contact.css'
 })
 export class ContactComponent {
+  onSubmit(event: Event) {
+    event.preventDefault(); // Stop native submission for now
 
+    const recaptchaSiteKey = "6LdXLW8rAAAAACKgzWk1OrlE0E8F7gGmRXvhB2MC";
+
+    grecaptcha.ready(() => {
+      grecaptcha.execute(recaptchaSiteKey, { action: 'submit' }).then((token: string) => {
+        // Send token to Firebase to verify
+        fetch('http://localhost:5001/Angular-dev-portfolio/us-central1/verifyRecaptcha', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ recaptchaToken: token })
+        })
+        .then(res => res.json())
+        .then(result => {
+          if (result.success && result.score >= 0.5) {
+            // reCAPTCHA validated - submit form now
+            (event.target as HTMLFormElement).submit();
+          } else {
+            alert('Sorry, we could not verify you as a real user.');
+          }
+        });
+      });
+    });
+  }
 }
